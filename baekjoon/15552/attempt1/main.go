b7dea5a31d280faa8a7fea8cc584c8dff2b0dffb
onee-only <kimww0306@gmail.com>
2023-05-31 14:51:27 +0900

    백준 15552

---
+++ baekjoon/15552/attempt1/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var length int
+
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	fmt.Fscanln(reader, &length)
+
+	var a, b int
+
+	for i := 0; i < length; i++ {
+		fmt.Fscanln(reader, &a, &b)
+		fmt.Fprintln(writer, a+b)
+	}
+	writer.Flush()
+}
