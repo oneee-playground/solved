85a67140d9afd4b85ed6feac1d23818dc2156ebb
김원욱 <kimww0306@gmail.com>
2024-01-31 22:09:12 +0900

    [Platinum V] Title: 히스토그램, Time: 52 ms, Memory: 7712 KB -BaekjoonHub

---
+++ 백준/Platinum/1725. 히스토그램/히스토그램.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type pair struct {
+	idx int
+	h   uint64
+}
+
+type stack []pair
+
+func (s stack) push(data pair) stack {
+	return append(s, data)
+}
+
+func (s stack) top() pair {
+	last := len(s) - 1
+	return s[last]
+}
+
+func (s stack) pop() (stack, pair) {
+	last := len(s) - 1
+	return s[:last], s[last]
+}
+
+func (s stack) empty() bool {
+	return len(s) == 0
+}
+
+func main() {
+	r := bufio.NewReader(os.Stdin)
+	w := bufio.NewWriter(os.Stdout)
+	defer w.Flush()
+
+	var (
+		n int
+
+		idx  int
+		s    stack
+		item pair
+
+		num, maxArea, width uint64
+	)
+
+	fmt.Fscan(r, &n)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(r, &num)
+		idx = i
+
+		for !s.empty() && s.top().h > num {
+			s, item = s.pop()
+			width = uint64(i)
+			if !s.empty() {
+				width -= uint64(item.idx)
+			}
+			maxArea = max(maxArea, item.h*width)
+			idx = item.idx
+		}
+		s = s.push(pair{idx: idx, h: num})
+	}
+
+	for !s.empty() {
+		s, item = s.pop()
+		width = uint64(n)
+		if !s.empty() {
+			width -= uint64(item.idx)
+		}
+		maxArea = max(maxArea, item.h*width)
+	}
+	fmt.Fprintln(w, maxArea)
+}
+
+func max(a, b uint64) uint64 {
+	if a > b {
+		return a
+	}
+	return b
+}
