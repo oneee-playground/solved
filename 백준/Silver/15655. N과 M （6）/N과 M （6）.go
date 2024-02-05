638dd163fd29985b2e773784738f0c26d5265986
김원욱 <kimww0306@gmail.com>
2024-02-05 15:29:12 +0900

    [Silver III] Title: N과 M (6), Time: 4 ms, Memory: 884 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"sort"
+)
+
+func main() {
+	r := bufio.NewReader(os.Stdin)
+	w := bufio.NewWriter(os.Stdout)
+	defer w.Flush()
+
+	fmt.Fscan(r, &n, &m)
+	nums = make([]int, n)
+	for i := range nums {
+		fmt.Fscan(r, &nums[i])
+	}
+	sort.Slice(nums, func(i, j int) bool {
+		return nums[i] < nums[j]
+	})
+
+	idxs = make([]int, m)
+	find(w, 0)
+}
+
+var (
+	n, m int
+	nums []int
+	idxs []int
+)
+
+func find(w io.Writer, i int) {
+	if i == m {
+		for _, idx := range idxs {
+			fmt.Fprintf(w, "%d ", nums[idx])
+		}
+		fmt.Fprintln(w)
+		return
+	}
+
+	j := 0
+	if i > 0 {
+		j = idxs[i-1] + 1
+	}
+	for ; j < n; j++ {
+		idxs[i] = j
+		find(w, i+1)
+	}
+}
