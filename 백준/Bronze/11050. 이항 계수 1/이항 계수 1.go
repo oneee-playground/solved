49e471acb63827d6c78b40fd408d27abac1d8925
김원욱 <kimww0306@gmail.com>
2024-02-09 13:43:15 +0900

    [Bronze I] Title: 이항 계수 1, Time: 4 ms, Memory: 872 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	r := bufio.NewReader(os.Stdin)
+	w := bufio.NewWriter(os.Stdout)
+	defer w.Flush()
+	var (
+		n, k int
+		dp   [][]int
+	)
+
+	fmt.Fscan(r, &n, &k)
+	dp = make([][]int, n+1)
+	for i := 1; i <= n; i++ {
+		dp[i] = make([]int, n+1)
+		dp[i][0], dp[i][i] = 1, 1
+		for j := 1; j < i; j++ {
+			dp[i][j] = (dp[i-1][j] + dp[i-1][j-1])
+		}
+	}
+	fmt.Fprintln(w, dp[n][k])
+}
