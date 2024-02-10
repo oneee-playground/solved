ada6441780af0f634c71a74e4ff05a7db51c6cdc
김원욱 <kimww0306@gmail.com>
2024-02-10 17:10:35 +0900

    [Silver II] Title: 과자 나눠주기, Time: 1052 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+)
+
+func main() {
+	r := bufio.NewReader(os.Stdin)
+	w := bufio.NewWriter(os.Stdout)
+	defer w.Flush()
+	var (
+		n, m int
+		arr  []int
+	)
+
+	fmt.Fscan(r, &m, &n)
+	arr = make([]int, n)
+	for i := range arr {
+		fmt.Fscan(r, &arr[i])
+	}
+
+	sort.Ints(arr)
+
+	var start, end int = 0, 1e9
+	for start < end {
+		mid := (start + end + 1) / 2
+		if ok(arr, m, mid) {
+			start = mid
+			continue
+		}
+		end = mid - 1
+	}
+	fmt.Fprintln(w, start)
+}
+
+func ok(arr []int, m, n int) bool {
+	cnt := 0
+	for _, num := range arr {
+		cnt += num / n
+	}
+	return cnt >= m
+}
