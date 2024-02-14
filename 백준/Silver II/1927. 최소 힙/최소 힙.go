5963e09d0978d5619ab7a8f4ecf36b5286478ca3
김원욱 <kimww0306@gmail.com>
2024-02-14 15:02:28 +0900

    [Silver II] Title: 최소 힙, Time: 76 ms, Memory: 5788 KB -BaekjoonHub

---
+++ 백준/Silver II/1927. 최소 힙/최소 힙.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"container/heap"
+	"fmt"
+	"os"
+)
+
+type minHeap []int
+
+func (m *minHeap) Len() int           { return len(*m) }
+func (m *minHeap) Less(i, j int) bool { return (*m)[i] < (*m)[j] }
+func (m *minHeap) Swap(i, j int)      { (*m)[i], (*m)[j] = (*m)[j], (*m)[i] }
+func (m *minHeap) Push(x any)         { *m = append(*m, x.(int)) }
+
+func (m *minHeap) Pop() any {
+	last := (*m)[m.Len()-1]
+	*m = (*m)[:m.Len()-1]
+	return last
+}
+
+func main() {
+	r := bufio.NewReader(os.Stdin)
+	w := bufio.NewWriter(os.Stdout)
+	defer w.Flush()
+	var (
+		n, v int
+		h    = new(minHeap)
+	)
+
+	fmt.Fscan(r, &n)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(r, &v)
+		if v == 0 {
+			v = 0
+			if h.Len() > 0 {
+				v = heap.Pop(h).(int)
+			}
+			fmt.Fprintln(w, v)
+			continue
+		}
+		heap.Push(h, v)
+	}
+}
