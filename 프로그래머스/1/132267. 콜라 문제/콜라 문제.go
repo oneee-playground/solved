435d450428dcf0078c6a6009cc0e1f6bc6ccdd2a
김원욱 <kimww0306@gmail.com>
2024-02-29 16:48:05 +0900

    [level 1] Title: 콜라 문제, Time: 0.00 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.go
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.go
@@ -2,7 +2,7 @@ func solution(a int, b int, n int) int {
     sum := 0
     for n >= a {
         rem := n % a
-        n = (b * (n / a))
+        n = b * (n / a)
         sum += n
         n += rem
     }
